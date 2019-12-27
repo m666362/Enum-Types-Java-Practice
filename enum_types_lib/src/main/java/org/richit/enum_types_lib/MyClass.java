390de71c52314109c0ee94aec3655aaa7ed2d460
Md Rayhan <mdrayhan9464@gmail.com>
2019-12-27 19:11:01 +0600

    enum Animal class added

--- enum_types_lib/src/main/java/org/richit/enum_types_lib/MyClass.java
+++ enum_types_lib/src/main/java/org/richit/enum_types_lib/MyClass.java
@@ -1,19 +1,21 @@
 package org.richit.enum_types_lib;
 
+import com.sun.org.apache.bcel.internal.generic.BREAKPOINT;
+
 import java.util.Calendar;
 
 public class MyClass {
     static String dot = "----------------";
     private static void startProgramme() {
         String start = "Programme started";
-        System.out.println("");
+        System.out.println();
         System.out.println(dot+start+dot);
-        System.out.println("");
+        System.out.println();
     }
     public static void endProgramme(){
 
         String end = "Programme ended";
-        System.out.println("");
+        System.out.println();
         System.out.println(dot+end+dot);
         System.out.println();
     }
@@ -22,7 +24,27 @@ public class MyClass {
         startProgramme();
 
         switchCase();
+        enim();
+
         endProgramme();
+    }
+
+    private static void enim() {
+        Animal animal = Animal.CAT;
+        switch (animal) {
+            case DOG:
+                System.out.println("dog");
+                break;
+            case CAT:
+                System.out.println("cat");
+                break;
+            case MOUSE:
+                System.out.println("mouse");
+                break;
+            default:
+                System.out.println("not found");
+                break;
+        }
     }
 
     private static void switchCase() {
